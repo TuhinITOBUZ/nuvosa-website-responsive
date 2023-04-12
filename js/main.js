731a9d0aed2a98bd56fff6eb2a6beab3a7828a9c
Tuhin Mukherjee <tuhin@itobuz.com>
2023-04-12 09:58:10 +0530

    update

--- js/main.js
+++ js/main.js
@@ -1,41 +1,40 @@
-const slides = document.querySelectorAll(".slider-card")
-const planCards = document.querySelectorAll(".plans-card")
-const planCardElements = document.querySelectorAll(".white-color")
-let slideCounter = 0
+const slides = document.querySelectorAll(".slider-card");
+const planCards = document.querySelectorAll(".plans-card");
+const planCardElements = document.querySelectorAll(".white-color");
+let slideCounter = 0;
 
 if (window.innerWidth < 960) {
   slides.forEach((slide, index) => {
-    slide.style.left = `${index * 70}%`
-  })
+    slide.style.left = `${index * 70}%`;
+  });
   document.getElementById("next-button").addEventListener("click", () => {
     if (slideCounter < 3) {
-      slideCounter = (slideCounter + 1)
-      slideSliderImage()
+      slideCounter = slideCounter + 1;
+      slideSliderImage();
     }
-  })
+  });
   document.getElementById("previous-button").addEventListener("click", () => {
     if (slideCounter > 0) {
-      slideCounter = (slideCounter - 1)
-      slideSliderImage()
+      slideCounter = slideCounter - 1;
+      slideSliderImage();
     }
-  })
-}
-else {
+  });
+} else {
   slides.forEach((slide, index) => {
-    slide.style.left = `${index * 50}%`
-  })
+    slide.style.left = `${index * 50}%`;
+  });
   document.getElementById("next-button").addEventListener("click", () => {
     if (slideCounter < 2) {
-      slideCounter = (slideCounter + 1)
-      slideSliderImage()
+      slideCounter = slideCounter + 1;
+      slideSliderImage();
     }
-  })
+  });
   document.getElementById("previous-button").addEventListener("click", () => {
     if (slideCounter > 0) {
-      slideCounter = (slideCounter - 1)
-      slideSliderImage()
+      slideCounter = slideCounter - 1;
+      slideSliderImage();
     }
-  })
+  });
 }
 
 const slideSliderImage = () => {
